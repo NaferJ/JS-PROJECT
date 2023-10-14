b8296d820b86022aa4f31f5d1131add9fc51ca68
NaferJ <naferjml@gmail.com>
2023-10-13 21:57:19 -0500

    Empezando

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'localhost',
+        user: 'root',
+        password: '1044607232',
+        database: 'db_links'
+    }
+
+};
